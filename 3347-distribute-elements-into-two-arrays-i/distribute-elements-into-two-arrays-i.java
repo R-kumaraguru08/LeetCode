de4ca6e801b6fea39c14e08cfc6d7d44771d7659
R-kumaraguru08 <rkumaraguru2023@gmail.com>
2026-08-20 21:04:30 +0530

    Time: 1 ms (98.08%) | Memory: 46.8 MB (42.79%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,27 +1,26 @@
 class Solution {
     public int[] resultArray(int[] nums) {
-        int i=0,j=1;
-        int count=0;
+        int[] out = new int[nums.length];
+        int count = 0,j=1,i=0;
         while(i<nums.length-1 && j<nums.length-1){
             if(nums[i]>nums[j]){
                 count++;
                 i=Math.max(i,j)+1;
             }
-            else
+            else{
                 j=Math.max(i,j)+1;
+            }
         }
-      
-        int[] op=new int[nums.length];
-        i=0;j=count+1;
-        op[0]=nums[0];
-        op[j]=nums[1];
+        j=count+1;i=0;
+        out[i] = nums[0];out[j] = nums[1];
         for(int k=2;k<nums.length;k++){
-            if(op[i]>op[j])
-                op[++i]=nums[k];
-            else
-                op[++j]=nums[k];
-
+            if(out[i]<out[j]){
+                out[++j] = nums[k];
+            }
+            else{
+                out[++i] = nums[k];
+            }
         }
-        return op;
+        return out;
     }
 }
