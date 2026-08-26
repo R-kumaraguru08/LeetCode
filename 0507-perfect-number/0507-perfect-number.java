6c8d7c557f5e7395ea20a1bec852a037b210781f
R-kumaraguru08 <rkumaraguru2023@gmail.com>
2026-08-26 22:20:29 +0530

    Time: 2082 ms (57.11%), Space: 42 MB (80.03%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -4,7 +4,11 @@ class Solution {
         for(int i=1;i<=num/2;i++){
             if(num%i==0){
                 sum+=i;
+            } 
+            if(sum>num) {
+                return false;
             }
+
         }
         return sum==num;
     }
